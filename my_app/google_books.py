3a09cdfde4b9a71b8fda6e50dc114c6212c4a39a
Arvind Fauzdar <arvindkumar4t456@gmail.com>
2023-07-13 10:43:58 +0530

    added related book in book detail

--- my_app/google_books.py
+++ my_app/google_books.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY = ' ' 
+API_KEY = 'AIzaSyA6J4OPdUsG488jDSec2RJi2gFOneWm_xA' 
 
 def search_books(query=None, max_results=24 , start_index=0,orderBy='relevance'):
     url = 'https://www.googleapis.com/books/v1/volumes'
@@ -24,7 +24,6 @@ def search_books(query=None, max_results=24 , start_index=0,orderBy='relevance')
             book_info = book['volumeInfo']
             title = book_info.get('title')
             authors = book_info.get('authors', [])
-            rating = book_info.get('averageRating')
             category = book_info.get('categories',[])
             book_id = book['id']
             # Extracting year of publication
@@ -62,6 +61,8 @@ def book_data(volume_id=None) :
     categories = book_info.get('categories', [])
     publisher = book_info.get('publisher')
     published_date = book_info.get('publishedDate')
+    rating = book_info.get('averageRating')
+    page_count = book_info.get('pageCount')
     thumbnail = book_info.get('imageLinks', {}).get('thumbnail')
     preview_link = book_info.get('previewLink')
     
@@ -74,7 +75,43 @@ def book_data(volume_id=None) :
         'published_date': published_date,
         'thumbnail': thumbnail,
         'preview_link': preview_link,
+        'rating' : rating ,
+        'page_count' : page_count ,
     }
     
     return book
 
+
+def get_related_books(book_id, max_results=12):
+    url = f'https://www.googleapis.com/books/v1/volumes/{book_id}/related-books'
+    params = {
+        'key': API_KEY,
+        'maxResults': max_results
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    related_books = []
+
+    if 'items' in data:
+        for item in data['items']:
+            book = item.get('volumeInfo')
+            if book:
+                title = book.get('title')
+                authors = book.get('authors', [])
+                description = book.get('description')
+                thumbnail = book.get('imageLinks', {}).get('thumbnail')
+                preview_link = book.get('previewLink')
+
+                book_data = {
+                    'title': title,
+                    'authors': authors,
+                    'description': description,
+                    'thumbnail': thumbnail,
+                    'preview_link': preview_link,
+                }
+
+                related_books.append(book_data)
+
+    return related_books
