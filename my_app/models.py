c15fc6fa7fa7581023e86f32514f23c4d184f3f1
Arvind Fauzdar <arvindkumar4t456@gmail.com>
2023-07-17 12:59:50 +0530

    ....

--- my_app/models.py
+++ my_app/models.py
@@ -7,13 +7,3 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    date_posted = models.DateTimeField(default=timezone.now)
-    pages = models.IntegerField()
-    review = models.TextField()
-    category = models.ManyToManyField(Category)
-
-    def __str__(self):
-        return self.title
