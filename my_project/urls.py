623b54ccee7749316c14e95f3d19b508c0140b64
Arvind Fauzdar <arvindkumar4t456@gmail.com>
2023-07-17 13:09:56 +0530

    ....

--- my_project/urls.py
+++ my_project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
-from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
